9eb88109df0fb5e53196a186ee4f00c49c135f84
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2022-04-22 12:46:50 +0300

    fix

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -2,7 +2,7 @@ const BASE_URL = 'https://jsonplaceholder.typicode.com'
 
 function fetchPosts() {
   
-  return fetch(`${BASE_URL}/posts?limit=10`).then(response => response.json());
+  return fetch(`${BASE_URL}/posts?_limit=10$_page=1`).then(response => response.json());
 }
 
 export default { fetchPosts };
